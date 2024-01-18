84db2fe3d6c5ad1637fb3e0b91a46b98413ffbe1
vintasoft <alex@vintasoft.com>
2024-01-18 11:48:59 +0300

    Source codes update

--- src/wwwroot/js/UI/Dialogs/ErrorMessageDialog.js
+++ src/wwwroot/js/UI/Dialogs/ErrorMessageDialog.js
@@ -3,41 +3,56 @@
 */
 ErrorMessageDialogJS = function (errorData) {
 
-    var errorMessage;
+    var errorMessage = "";
     var additionalErrorInfo = null;
     if (typeof errorData == "string") {
         errorMessage = errorData;
     }
     else if (typeof errorData == "object") {
-        if (errorData.errorMessage != undefined) {
-            errorMessage = errorData.errorMessage;
+        var errors = errorData.errors;
+        if (errors != undefined) {
+            var errors = errors[''];
+            if (errors != null) {
+                for (var i = 0; i < errors.length; i++) {
+                    if (errorMessage != "") {
+                        errorMessage += "\n";
+                    }
+                    errorMessage += errors[i];
+                }
+            }
         }
-        else if (errorData.message != undefined) {
-            errorMessage = errorData.message;
-        }
-        else if (errorData.error != undefined) {
-            errorMessage = errorData.error;
-        }
-        else if (errorData.responseJSON != undefined) {
-            if (errorData.responseJSON.Message != null && errorData.responseJSON.MessageDetail != null) {
-                errorMessage = errorData.responseJSON.Message;
-                additionalErrorInfo = errorData.responseJSON.MessageDetail;
+
+        if (errorMessage == "") {
+            if (errorData.errorMessage != undefined) {
+                errorMessage = errorData.errorMessage;
+            }
+            else if (errorData.message != undefined) {
+                errorMessage = errorData.message;
+            }
+            else if (errorData.error != undefined) {
+                errorMessage = errorData.error;
+            }
+            else if (errorData.responseJSON != undefined) {
+                if (errorData.responseJSON.Message != null && errorData.responseJSON.MessageDetail != null) {
+                    errorMessage = errorData.responseJSON.Message;
+                    additionalErrorInfo = errorData.responseJSON.MessageDetail;
+                }
+                else {
+                    errorMessage = errorData.responseJSON.ExceptionMessage;
+                }
+            }
+            else if (errorData.responseText != undefined) {
+                if (errorData.responseText.Message != undefined && errorData.responseText.MessageDetail != undefined) {
+                    errorMessage = errorData.responseText.Message;
+                    additionalErrorInfo = erorData.responseText.MessageDetail;
+                }
+                else {
+                    errorMessage = errorData.responseText;
+                }
             }
             else {
-                errorMessage = errorData.responseJSON.ExceptionMessage;
+                errorMessage = "Unknown error";
             }
-        }
-        else if (errorData.responseText != undefined) {
-            if (errorData.responseText.Message != undefined && errorData.responseText.MessageDetail != undefined) {
-                errorMessage = errorData.responseText.Message;
-                additionalErrorInfo = erorData.responseText.MessageDetail;
-            }
-            else {
-                errorMessage = errorData.responseText;
-            }
-        }
-        else {
-            errorMessage = "Unknown error";
         }
     }
 
