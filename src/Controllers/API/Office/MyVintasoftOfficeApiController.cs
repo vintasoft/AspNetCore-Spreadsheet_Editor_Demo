8c2faeb96e5cd45e7bc76441d534b9ca4646d13e
vintasoft <alex@vintasoft.com>
2026-06-26 12:37:21 +0300

    Source codes update

--- src/Controllers/API/Office/MyVintasoftOfficeApiController.cs
+++ src/Controllers/API/Office/MyVintasoftOfficeApiController.cs
@@ -16,6 +16,8 @@ namespace AspNetCoreSpreadsheetEditorDemo.Controllers
         public MyVintasoftOfficeApiController(IWebHostEnvironment hostingEnvironment)
             : base(hostingEnvironment)
         {
+            // specify that converter from CSV/TSV file to XLSX file should automatically calculate the column widths
+            Vintasoft.Imaging.Codecs.Decoders.XlsxConverterSettings.Default.ColumnAutoFit = true;
         }
 
     }
